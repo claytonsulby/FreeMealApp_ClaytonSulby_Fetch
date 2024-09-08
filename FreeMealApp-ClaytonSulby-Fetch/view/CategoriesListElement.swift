0076b552b03a99492c20af61be2e8290a57f74f8
Clayton Sulby <48489392+claytonsulby@users.noreply.github.com>
2024-09-07 17:10:56 -0700

    detailed category row for top level

---
+++ FreeMealApp-ClaytonSulby-Fetch/view/CategoriesListElement.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesListElement.swift
+//  FreeMealApp-ClaytonSulby-Fetch
+//
+//  Created by Clayton Sulby on 9/7/24.
+//
+
+import SwiftUI
+
+struct CategoriesListElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesListElement()
+}
